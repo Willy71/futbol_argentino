6903b10dfad8319c4697256048a2f7087a9475b3
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-14 08:19:00 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,12 +3,7 @@ import pandas as pd
 import webbrowser
 from datetime import datetime
 
-if "data" not in st.session_state:
-    df_data = pd.read_csv("datasets\_futbol_argentino_logos.csv")
-    df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
-    df_data = df_data[df_data["Value(£)"] > 0]
-    df_data = df_data.sort_values(by="Overall", ascending = False)
-    st.session_state["data"] = df_data
+df_data = pd.read_csv("datasets\_futbol_argentino_logos.csv")
 
 # Colocar nome na pagina, icone e ampliar a tela
 st.set_page_config(
