5f7fbcd3c0cd48213969b60e9f66face69289227
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-18 22:13:32 -0300

    Update Equipos.py

--- pages/Equipos.py
+++ pages/Equipos.py
@@ -8,6 +8,18 @@ st.set_page_config(
     layout="wide"
 )
 
+# We reduced the empty space at the beginning of the streamlit
+reduce_space ="""
+            <style type="text/css">
+            /* Remueve el espacio en el encabezado por defecto de las apps de Streamlit */
+            div[data-testid="stAppViewBlockContainer"]{
+                padding-top:30px;
+            }
+            </style>
+            """
+# We load reduce_space
+st.html(reduce_space)
+
 # Check if you've already initialized the data
 if 'df' not in st.session_state:
     # Get the data if you haven't
